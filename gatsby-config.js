29f5a257f9cbd21864b7e47eed4e396fc802fc55
Felipe Uliana <v.uliana@gmail.com>
2021-03-05 13:04:02 -0300

    chore: Adjust and format config files
    Add 'favicon' icon too.
    
    https://github.com/felipeuliana/brutus-blog/projects/1#card-56274905

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,9 @@ const path = require('path')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs'
+    author: 'Felipe Uliana "Brutus"',
+    description: 'My blog about web development.',
+    title: 'Brutus Blog'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -22,13 +21,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#011627',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+        name: 'Brutus Blog',
+        short_name: 'Brutus',
+        start_url: '/',
+        theme_color: '#011627'
       }
     },
     'gatsby-plugin-gatsby-cloud'
